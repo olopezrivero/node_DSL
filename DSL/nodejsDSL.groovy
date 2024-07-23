a188a342e116b1826442069e5c90a8398cbffed1
olopezrivero <119618239+olopezrivero@users.noreply.github.com>
2024-07-23 16:05:19 -0300

    Update nodejsDSL.groovy

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,13 +1,10 @@
-job('Aplicacion Node.js DSL') {
-    description('Aplicación Node JS DSL para el curso de Jenkins')
+job('Java Maven App DSL') {
+    description('Java Maven App con DSL para el curso de Jenkins')
     scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+        git('https://github.com/olopezrivero/Java_DSL.git', 'master') { node ->
+            node / gitConfigName('olopezrivero')
+            node / gitConfigEmail('olopezrivero@zeni.com.ar')
         }
-    }
-    triggers {
-        scm('H/7 * * * *')
     }
     wrappers {
         nodejs('nodejs')
@@ -15,23 +12,23 @@ job('Aplicacion Node.js DSL') {
     steps {
         shell("npm install")
     }
-    publishers {
-	slackNotifier {
-            notifyAborted(true)
-            notifyEveryFailure(true)
-            notifyNotBuilt(false)
-            notifyUnstable(false)
-            notifyBackToNormal(true)
-            notifySuccess(true)
-            notifyRepeatedFailure(false)
-            startNotification(false)
-            includeTestSummary(false)
-            includeCustomMessage(false)
-            customMessage(null)
-            sendAs(null)
-            commitInfoChoice('NONE')
-            teamDomain(null)
-            authToken(null)
+    properties {
+        office365ConnectorWebhooks {
+            webhooks {
+                webhook {
+                    name('Jenkins-DSL')
+                    url('https://enriquezeni.webhook.office.com/webhookb2/920c20af-d214-4648-b537-105f2efeffec@9d616da3-f444-48df-aea4-ae00aeb3403e/JenkinsCI/0085251523d94bc587b316e9895d17f4/1c549eee-f4e1-479b-89fd-96e25e656e91')
+                    startNotification(false)
+                    notifySuccess(true)
+                    notifyAborted(false)
+                    notifyNotBuilt(false)
+                    notifyUnstable(true)
+                    notifyFailure(true)
+                    notifyBackToNormal(true)
+                    notifyRepeatedFailure(false)
+                    timeout(30000)
+                }
+            }
         }
     }
 }
